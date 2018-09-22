6edd58876f8078029256e716c7d702619ec84bdc
Caroline Lin <ruoyi.lin@gmail.com>
2018-09-22 12:21:36 -0400

    Set up react, jest, enzyme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 // @flow
 
-const x : number = 5;
-console.log("Hello world?");
+import React from "react"
+import ReactDOM from "react-dom";
 
+export class Index extends React.Component<{}>{
+
+    render(){
+        return <p>Hello world?</p>
+    }
+
+};
+
+if (!module.parent){
+    var el = document.getElementById("root")
+    if (el === null){
+        throw new Error("Couldn't load root element!");
+    } else{
+        ReactDOM.render(<Index />, el);
+    }
+    
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,18 @@
 // @flow
+import { mount, configure } from "enzyme";
+import { Index } from "./index";
+import React from "react"
+
+// setup file
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+describe("Index", () => {
+  it("shows 'Hello world?'", () => {
+    const index = mount(<Index></Index>);
+    expect(index.text()).toEqual("Hello world?");
+  });
+});
 
 it("dummy test", () => {
   expect(true).toEqual(true);
