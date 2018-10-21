d66a779c316eda8791ffca0c87ace302f8eac112
Caroline Lin <ruoyi.lin@gmail.com>
2018-10-21 11:10:16 -0400

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+var path = require('path');
+
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 8080
+  },
   module: {
     rules: [
       {
